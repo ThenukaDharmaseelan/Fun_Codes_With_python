1de47a357981a70e83061517c0b091f1a903f1dc
Thenuka Dharmaseelan <40541428+ThenukaDharmaseelan@users.noreply.github.com>
2020-12-26 07:35:25 +0530

    Helloween_prank

---
+++ halloween-prank/Helloween Prank.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pygame
+
+
+# In[2]:
+
+
+from time import sleep
+
+
+# In[3]:
+
+
+pygame.init()
+window = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+pygame.mixer.init()
+pygame.mixer.music.load('ratsasan.mp3')
+pygame.mixer.music.play()
+sleep(5)
+pygame.mixer.music.load('scary.mp3')
+pygame.mixer.music.play()
+sleep(1)
+image = pygame.image.load('scr.jpg')
+window.blit(image,(0,0))
+pygame.display.update()
+sleep(3)
+
